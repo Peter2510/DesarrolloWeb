dee6b0dbe79bd057ea1cae3baef33039d863f0cb
Pedro Gordillo <pedrogordillo2510@gmail.com>
2021-12-08 02:10:31 -0600

    Paginas agregadas, se tiene funcionalidad de login

---
+++ js/ingresar.js
@@ -0,0 +1,6 @@
+function ingresar(email,pass){
+    let correo = document.getElementById("email").value;
+    let contraseña = document.getElementById("pass").value;
+    
+    return "El correo electronico es: " + correo + " y la contraseña es: " + contraseña;
+}
