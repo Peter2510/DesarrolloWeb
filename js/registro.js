a48d51cd03374120a69e4ee68cbbf8651daf6ffe
Pedro Gordillo <pedrogordillo2510@gmail.com>
2021-12-09 00:32:02 -0600

    funcionalidad de registro agregada 2.0

--- js/registro.js
+++ js/registro.js
@@ -1,12 +1,18 @@
-function registro(mail,pass1,pass2){
+function registro(nombrelet,apellidolet,mail,pass1,pass2){
 
+    let nombre = document.getElementById("nombre").value;
+    let apellido = document.getElementById("apellido").value;
     let correo = document.getElementById("mail").value;
     let contrasenia1 = document.getElementById("pass1").value;
     let contrasenia2 = document.getElementById("pass2").value;
 
-    if(correo.length>0 && contrasenia1.length>0 && contrasenia2.length>0){
+    if(nombre.length>0&&apellido.length>0&&correo.length>0 && contrasenia1.length>0 
+        && contrasenia2.length>0){
         if(contrasenia1==contrasenia2){
-            return "Usuario registrado con exito, con correo " + correo + " y contraseña " + contrasenia1;
+            return "Usuario registrado con exito\n" + 
+            "nombre: " + nombre + "\napellido: " + apellido + "\ncorreo electronico: " + correo
+            + "\ncontraseña: " + contrasenia1;
+            
 
         }else{
             return "Las contraseñas no coinciden";
